0b3c96d3380545808e9e5713e8906ff84634dd20
AndreeaParvu <andreea.stoica1991@gmail>
2022-11-11 13:06:53 +0200

    inital order components

---
+++ src/features/checkout/checkoutValidation.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+export const validationSchema = yup.object({
+    fullName: yup.string().required('Full name is required'),
+    address1: yup.string().required('Address line 1 is required'),
+    address2: yup.string().required('Address line 2 is required'),
+    city: yup.string().required(),
+    zipCode: yup.string().required(),
+    country: yup.string().required()
+});
